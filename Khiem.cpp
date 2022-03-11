4729904fea08e4701d3a282bbe021a1aa4fa7206
Duy Khiem <duykhiemmylove2003@gmail.com>
2022-03-11 13:27:56 +0700

    update

--- Khiem.cpp
+++ Khiem.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 int main() {
-for (int i = 0; i < n; i++)
-   cout << i << endl;
-return 0;
+	for (int i = 0; i < 10; i++)
+		cout << i << endl;
+	system("pause");
 }
